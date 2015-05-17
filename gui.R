af6f6f7559157be2417a47ff69146b052e069cd1
Edward Roche <eroche@eds-computer-fedora.net>
2015-05-17 14:58:15 +0100

    Initial Commit

---
+++ gui.R
@@ -0,0 +1,6 @@
+## The purpose of this GUI is to let the user
+## 1. Specify a file to load
+## 2. Let user choose the variable types and edit names if desired
+## 3. Load the data to a R data structure (data frame or data table)
+## Any further functionality will be provided in further GUI implementations
+library(RGtk2)
