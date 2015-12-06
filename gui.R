eb9c7bb0f3d82a9dda40e826bfd10a00092ce058
Ed Roche <ed.roche@yahoo.co.uk>
2015-12-06 15:14:55 +0000

    Added Date Variable Type to import

--- gui.R
+++ gui.R
@@ -9,12 +9,14 @@ options("guiToolkit"="tcltk")
 ## This program lets the user select a csv file to load and choose the variables to import and the types ----
 ##############################################################################################################
 ## The file is designed to create an environment to store the various overhead need to import the file
-## This has not been designed to be especially efficient and is currently only function and is intended
+## This has not been designed to be especially efficient and is currently only intended
 ## to be a proof of concept rather than a production program
 ## The first section will let the user pick the data to load
-## The second section will show the loaded data variables and their current
-## formats etc.
-
+## The second section will present the loaded data variables and their current
+## formats etc in a GUI
+## The user can then select which variables to import from the file and add a data type to them
+## This GUI does not contain any error handling. This needs extensive error handling e.g. only allow
+## one origin etc
 ## Structure ----
 ## The purpose of this GUI is to let the user
 ## 1. Specify a file to load
@@ -32,9 +34,9 @@ options("guiToolkit"="tcltk")
 ## version of the program.
 env <- new.env()
 
-## Variable types
-env$types <- c("integer", "character", "factor")
-env$act_types <- c("incurred", "paid", "number","origin", "development", "class")
+## Variable types (R native types and Actuarial Types typical for importing Actuarail data)
+env$types <- c("integer", "character", "factor", "date")
+env$act_types <- c("Incurred", "Paid", "Count","Origin", "Development", "Class")
 
 ## Variable names and selected types
 env$z_names <- list()
@@ -78,14 +80,27 @@ env$selectData <- function(x,...) {
     ## Frame to store the variable names and types,
     ## add groups for the variable names and types and only make this visible after a
     ## file has been selected
-    env$data <- gframe("Chosse data to import and format", container=env$win, horizontal=T)
-    env$var_name <- gframe("Variable", container=env$data)
-    env$var_type <- gframe("Data Type", container=env$data)
-    env$var_act_type <- gframe("Actuarial Type", container=env$data)
+    env$data <- gframe("Chosse data to import and format", container=env$win, horizontal=F)
+    helpTxt <- "Select variables to load from import file
+Select Actuarial Type/Role. The raw data variables will be renamed accordingly
+Select Import data type which is native R type used when importing (Choose character if not sure or if you get an error)
+     Factors should be imported as factors
+     Dates (European format) should be imported as dates
+     Everything else can be impoted as a character
+"
+    glabel(helpTxt, container=env$data)
+    env$table <- gframe( container=env$data, horizontal=T)
+    env$var_name <- gframe("Variable", container=env$table)
+    env$var_act_type <- gframe("Actuarial Type", container=env$table)
+    env$var_type <- gframe("Import Type (R Type)", container=env$table)
 
+
+    
+    
     ## Names will be checkboxes initially so that you can select which fields to keep
 
     ## types will be radio boxes so that you can specify the type for import
+    ## Both R data types and Actuarial data types will be sepcified
     env$variables_names_list <- ggroup(container=env$var_name, horizontal=F)
     env$variables_data_type_list <- ggroup(container=env$var_type, horizontal=F)
 
@@ -105,10 +120,26 @@ env$selectData <- function(x,...) {
     ## Button to start the import ----
     env$file_button2 <- gbutton("Import Data", handler = 
                                     function(h, ...) {
+                                        ## sub date for character for import then convert to required date format
+                                        ## not possible to specify date format in read.csv
+                                        ## remove complexity of dates from the user
+                                        ## create  a local variable to store this
+                                        z_types <- gsub("date", "character",sapply(env$z_types, svalue))
                                         col.names.logical <- sapply(env$z_names, svalue)
-                                        col.classes <- sapply(env$z_types, svalue)[col.names.logical]
+                                        ## Drop unselected cols
+                                        col.classes <- ifelse(col.names.logical == FALSE,"NULL" , z_types)
                                         env$raw.data <- read.csv(file=env$data_filepath, colClasses=col.classes)
-                                        env$raw.data <- env$raw.data[,col.names.logical]
+                                        ## rename the variables
+                                        names(env$raw.data) <- sapply(env$z_act_types, svalue)[col.names.logical]
+                                        ##env$raw.data <- env$raw.data[,which(col.names.logical)==1]
+                                        ## Format dates
+                                        z_types <- sapply(env$z_types,svalue)[col.names.logical]
+                                        for( i in 1:length(z_types) ){
+                                            if(z_types[i] == "date") {
+                                                env$raw.data[,i] <- as.Date(env$raw.data[,i],"%d/%m/%Y")
+                                            }
+                                                     }
+                                        #env$raw.data <- env$raw.data[,col.names.logical]
                                         assign("raw.data", env$raw.data, envir=.GlobalEnv)
                                         print(nrow(env$raw.data))
                                         print(" rows imported")
